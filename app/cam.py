013c0936c68f31711b1efc84a846deb275ee4c5f
shuowww <shuowww@gmail.com>
2017-04-09 14:59:29 +0800

    finish a simple cam implemented by cv&pyqt4

---
+++ app/cam.py
@@ -0,0 +1,162 @@
+#!/usr/bin/env python2
+import cv2
+import numpy as np
+import sys
+
+import simple_classifier
+
+from PyQt4 import QtGui
+from PyQt4 import QtCore
+
+
+class OpenCVQImage(QtGui.QImage):
+
+    def __init__(self, bgrImg):
+        h, w = bgrImg.shape[:2]
+        rgbImg = cv2.cvtColor(bgrImg, cv2.COLOR_BGR2RGB)
+        self._imgData = rgbImg.tostring()
+        super(OpenCVQImage, self).__init__(self._imgData, w, h, \
+            QtGui.QImage.Format_RGB888)
+
+
+class CameraDevice(QtCore.QObject):
+
+    _DEFAULT_FPS = 30
+
+    newFrame = QtCore.pyqtSignal(np.ndarray)
+
+    def __init__(self, cameraId=0, parent=None):
+        super(CameraDevice, self).__init__(parent)
+
+
+        self._cameraDevice = cv2.VideoCapture(cameraId)
+        self._cameraDevice.set(3, 1500)
+        self._cameraDevice.set(4, 1300)
+
+        self._timer = QtCore.QTimer(self)
+        self._timer.timeout.connect(self._queryFrame)
+        self._timer.setInterval(1000/self._DEFAULT_FPS)
+
+        self.paused = False
+
+    @QtCore.pyqtSlot()
+    def _queryFrame(self):
+        ret, frame = self._cameraDevice.read()
+        mirroredFrame = frame.copy()
+        mirroredFrame = cv2.flip(frame, 1)
+        self.newFrame.emit(mirroredFrame)
+
+    @property
+    def paused(self):
+        return not self._timer.isActive()
+
+    @paused.setter
+    def paused(self, p):
+        if p:
+            self._timer.stop()
+        else:
+            self._timer.start()
+
+    @property
+    def frameSize(self):
+        w = self._cameraDevice.get(3)
+        h = self._cameraDevice.get(4)
+        return int(w), int(h)
+
+
+class CameraWidget(QtGui.QWidget):
+
+    newFrame = QtCore.pyqtSignal(np.ndarray)
+
+    predictFrame = QtCore.pyqtSignal(np.ndarray)
+
+    def __init__(self, cameraDevice, parent=None):
+        super(CameraWidget, self).__init__(parent)
+
+        self._frame = None
+
+        self._cameraDevice = cameraDevice
+        self._cameraDevice.newFrame.connect(self._onNewFrame)
+
+        w, h = self._cameraDevice.frameSize
+        self.setMinimumSize(w, h)
+        self.setMaximumSize(w, h)
+
+    @QtCore.pyqtSlot(np.ndarray)
+    def _onNewFrame(self, frame):
+        self._frame = frame.copy()
+        self.newFrame.emit(self._frame)
+        self.predictFrame.emit(self._frame)
+        self.update()
+
+    def changeEvent(self, e):
+        if e.type() == QtCore.QEvent.EnabledChange:
+            if self.isEnabled():
+                self._cameraDevice.newFrame.connect(self._onNewFrame)
+            else:
+                self._cameraDevice.newFrame.disconnect(self._onNewFrame)
+
+    def paintEvent(self, e):
+        if self._frame is None:
+            return
+        painter = QtGui.QPainter(self)
+        painter.drawImage(QtCore.QPoint(0, 0), OpenCVQImage(self._frame))
+
+class wholeWidget(QtGui.QWidget):
+    def __init__(self, camWidget):
+        super(wholeWidget, self).__init__()
+        self._camWidget = camWidget
+        self.initUI()
+
+    def initUI(self):
+        grid = QtGui.QGridLayout()
+        grid.setSpacing(5)
+
+        predictBtt = QtGui.QPushButton("predict")
+        predictBtt.clicked.connect(self._predictFunc)
+        trainBtt = QtGui.QPushButton("train")
+        grid.addWidget(predictBtt, 0, 0)
+        grid.addWidget(trainBtt, 0, 1)
+
+        grid.addWidget(self._camWidget, 1, 0, 5, 2)
+
+        self.setLayout(grid)
+
+        self.setGeometry(300, 300, 350, 300)
+
+    @Qtcore.pyqtSlot()
+    def _predictFunc(self):
+        self._camWidget.predictFrame.connect(_prect)
+
+    @Qtcore.pyqtSlot(np.ndarray)
+    def _predict(self, predictFrame):
+
+
+def main():
+
+#    @QtCore.pyqtSlot(cv.iplimage)
+#    def onNewFrame(frame):
+#        cv.CvtColor(frame, frame, cv.CV_RGB2BGR)
+#        msg = "processed frame"
+#        font = cv.InitFont(cv.CV_FONT_HERSHEY_DUPLEX, 1.0, 1.0)
+#        tsize, baseline = cv.GetTextSize(msg, font)
+#        w, h = cv.GetSize(frame)
+#        tpt = (w - tsize[0]) / 2, (h - tsize[1]) / 2
+#        cv.PutText(frame, msg, tpt, font, cv.RGB(255, 0, 0))
+
+    app = QtGui.QApplication(sys.argv)
+
+    cameraDevice = CameraDevice()
+
+#    cameraWidget1 = CameraWidget(cameraDevice)
+#    cameraWidget1.newFrame.connect(onNewFrame)
+#    cameraWidget1.show()
+
+    cameraWidget1 = CameraWidget(cameraDevice)
+    #cameraWidget1.show()
+    theWidget = wholeWidget(cameraWidget1)
+    theWidget.show()
+    sys.exit(app.exec_())
+
+if __name__ == '__main__':
+    main()
